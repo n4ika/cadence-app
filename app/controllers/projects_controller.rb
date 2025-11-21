508172f699605b1271f8062057c4d23d4e8e9f69
Naika Estriplet <naika.c.3113@gmail.com>
2025-11-21 01:17:22 -0500

    Add Dashboard, Projects, and Tasks controllers with CRUD logic and authorization

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,43 @@
+class ProjectsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_project, only: [ :edit, :update ]
+  before_action :authorize_project!, only: [ :edit, :update ]
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = current_user.projects.build(project_params)
+    if @project.save
+      redirect_to root_path, notice: "Project created successfully"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to root_path, notice: "Project updated successfully"
+    else
+      render :edit
+    end
+  end
+end
+
+private
+
+def project_params
+  params.require(:project).permit(:title, :client, :project_type, :start_date, :estimated_end_date)
+end
+
+def set_project
+  @project = Project.find(params[:id])
+end
+
+def authorize_project!
+  redirect_to root_path unless @project.user == current_user
+end
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,53 @@
+class TasksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_project
+  before_action :authorize_project!
+  before_action :set_task, only: [ :edit, :update, :destroy ]
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = @project.tasks.build(task_params)
+    if @task.save
+      redirect_to root_path, notice: "Task created successfully"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to root_path, notice: "Task updated successfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to root_path, notice: "Task deleted successfully"
+  end
+end
+
+private
+
+def task_params
+  params.require(:task).permit(:title, :description, :due_date, :status)
+end
+
+def set_project
+  @project = Project.find(params[:project_id])
+end
+
+def set_task
+  @task = Task.find(params[:id])
+end
+
+def authorize_project!
+  redirect_to root_path unless @project.user == current_user
+end
