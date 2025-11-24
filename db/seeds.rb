f409929f7b86f7717bde8bd7c4a84b1070b0f465
Naika Estriplet <naika.c.3113@gmail.com>
2025-11-23 22:29:57 -0500

    Add project form partial and display project details on dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,48 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user = User.create(
+  first_name: "Test",
+  last_name: "User",
+  email: "test@testemail.com",
+  password: "password123",
+  password_confirmation: "password123",
+  email_notifications_enabled: false,
+  in_app_notifications_enabled: true,
+  high_priority_notifications_only: false
+)
+
+project1 = user.projects.create(
+  title: "Website Migration",
+  client: "Test Co"
+)
+
+task1 = project1.tasks.create(
+  title: "Research Platforms",
+  due_date: "2026-01-01"
+)
+
+task2 = project1.tasks.create(
+  title: "Write project brief",
+  description: "create docuement for client with prices and timelines",
+  due_date: "2025-11-30"
+)
+
+project2 = user.projects.create(
+  title: "New Website Build",
+  client: "Ant Company"
+)
+
+task3 = project2.tasks.create(
+  title: "Write project scope",
+  due_date: "2025-11-26",
+  status: "in_progress"
+)
+
+task4 = project2.tasks.create(
+  title: "create brand guidelines",
+  due_date: "2025-11-30"
+)
+
+task5 = project2.tasks.create(
+  title: "source images and content",
+  due_date: "2025-11-30"
+)
+
