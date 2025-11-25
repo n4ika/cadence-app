764c92aa4ce1e7ab7dfaba7bc457f0b4f66e6546
Naika Estriplet <naika.c.3113@gmail.com>
2025-11-24 23:01:03 -0500

    Fix seed production initializer to load after models

--- config/initializers/seed_production.rb
+++ config/initializers/seed_production.rb
@@ -1,3 +1,8 @@
-if Rails.env.production? && User.count == 0
-  load(Rails.root.join("db", "seeds.rb"))
+if Rails.env.production?
+  Rails.application.config.after_initialize do
+    User.connection.execute("SELECT 1") # Ensure DB connection
+    if User.count == 0
+      load(Rails.root.join("db", "seeds.rb"))
+    end
+  end
 end
