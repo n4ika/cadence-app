cf585e3c4b66b2c7669633cfd0851e559969ece6
Naika Estriplet <naika.c.3113@gmail.com>
2025-11-25 18:12:32 -0500

    Fix nested tag routes to resolve deployment issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root "dashboard#index"
 
-  resources :projects, only: [ :new, :create, :edit, :update ] do
+  resources :projects, only: [ :new, :create, :edit, :update ]
+  resources :projects, only: [] do
     resources :tasks, only: [ :new, :create, :edit, :update, :destroy ]
   end
 
